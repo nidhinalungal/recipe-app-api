eadd618377ca6aeee9a2ffd519d6cf4f9a2fbce1
Nidhin Alungal <nidhinalungal.nalinan@dbizsolution.com>
2024-06-05 16:52:11 +0530

    fix linting.

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,18 +2,22 @@
 Calculator Function
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add x and y and return result."""
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """Subtract x from y and return result."""
     return y - x
 
-def multiply(x,y):
-    # Multiply x & y and return result.
-    return x * y 
 
-def devide(x,y):
+def multiply(x, y):
+    # Multiply x & y and return result.
+    return x * y
+
+
+def devide(x, y):
     # Devide x by y and return result.
     return(x / y)
